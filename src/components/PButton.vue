fdfb6639fc2ddfba3e768b7667bcc56b6b24c48f
ajomuch92 <aaronmontes_92@yahoo.es>
2022-09-07 19:19:10 -0600

    feat: 🎸 Creating button component

---
+++ src/components/PButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="tag">
+    <slot name="default"/>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'PButton',
+  props: {
+    tag: {
+      type: String,
+      default: 'button',
+      validator: (val) => ['button', 'a', 'input'].includes(val)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
